a85205629d784b4241b1cc37963c130c18b92e54
Victor Rolack <victorolack@gmail.com>
2023-09-04 21:15:44 -0300

    antes de insertar por lotes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
@@ -17,6 +19,10 @@ import { CommonModule } from './common/common.module';
     PokemonModule,
 
     CommonModule,
+
+    SeedModule,
+
+    HttpModule,
   ],
   controllers: [],
   providers: [],
